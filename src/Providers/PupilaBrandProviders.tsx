c0430d570810acc64517d6c14084ec2741847326
Sheilla Silva <sheilla.silva@contasimples.com>
2025-03-26 11:15:27 -0300

    feat: add theme

--- src/Providers/PupilaBrandProviders.tsx
+++ src/Providers/PupilaBrandProviders.tsx
@@ -1,9 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeUIProvider } from "theme-ui";
+import { theme } from "./theme";
 
 export function PupilaBrandProviders({ children }) {
   const queryClient = new QueryClient()
-
+  
   return(
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <ThemeUIProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ThemeUIProvider>
+
+    
   )
 }
