0fb47d142ac77ce37dfe5454df2ec7a2135f418c
Sheilla Silva <sheilla.silva@contasimples.com>
2025-03-26 07:21:15 -0300

    chore: add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,48 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["@typescript-eslint", "import", "unused-imports", "prettier"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
+      "@typescript-eslint/no-magic-numbers": [
+        "error",
+        {
+          ignoreEnums: true,
+          ignoreNumericLiteralTypes: true,
+          ignoreReadonlyClassProperties: true,
+          ignore: [0, 1, -1],
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            ["builtin", "external"],
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type"
+          ],
+          "newlines-between": "always",
+          "alphabetize": { "order": "asc", "caseInsensitive": true }
+        }
+      ],
+      "import/no-unresolved": "error",
+      "import/no-duplicates": "error",
+      "import/newline-after-import": "error",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+      ],
+      "prettier/prettier": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "error",
+      "@next/next/no-html-link-for-pages": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
